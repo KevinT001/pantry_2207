de674f4f98754770517803f71e1b8e1fd60884ef
Kevin Ta <kvnta@yahoo.com>
2022-08-02 09:40:50 -0600

    finish test for iteration 1

--- spec/pantry_spec.rb
+++ spec/pantry_spec.rb
@@ -4,6 +4,8 @@ require './lib/pantry'
 RSpec.describe Pantry do 
   before :each do 
     @pantry = Pantry.new
+     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
   end
 
   it 'exists' do 
@@ -15,8 +17,15 @@ RSpec.describe Pantry do
   end
 
   it 'can check stock' do 
-    expect(@pantry.stock_check(ingredient1).to eq 0)
+    expect(@pantry.stock_check(@ingredient1).to eq 0)
   end
 
-  
+  it 'can restock ingredients and check current stock' do 
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    expect(@pantry.stock_check(@ingredient1)).to eq(15)
+    @pantry.restock(@ingredient2, 7)
+    expect(@pantry.stock_check(ingredient2)).to eq(7)
+  end
+
 end
