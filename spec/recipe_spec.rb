bb8b6b0d386941de806b1461f2df4ae4463de55e
Kevin Ta <kvnta@yahoo.com>
2022-08-02 10:15:33 -0600

    Test:Ingredients required

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -6,5 +6,25 @@ RSpec.describe Recipe do
   before :each do 
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @recipe1 = Recipe.new("Mac and Cheese")
   end
+
+  it 'has dem attributes' do 
+    expect(@recipe1.name).to eq("Mac and Cheese")
+  end
+
+  it 'can list ingredients required as Hash and add to hash' do 
+    expect(@recipe1.ingredients_required).to eq({})
+
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    expect(@recipe1.ingredients_required).to eq({@ingredient1,@ingredient2})
+    
+  end
+
+
+
+
 end
