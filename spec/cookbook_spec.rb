30376096f7bc9c5431b0b8763b39c1c1fc5e22a6
Kevin Ta <kvnta@yahoo.com>
2022-08-02 10:44:24 -0600

    Test: Cookbook before each setup, and exists tests

---
+++ spec/cookbook_spec.rb
@@ -0,0 +1,22 @@
+require './lib/ingredient'
+require './lib/pantry'
+require './lib/recipe'
+require './lib/cook_book'
+
+RSpec.describe Cookbook do 
+  before :each do 
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @pantry = Pantry.new
+    @cookbook = CookBook.new
+
+  end
+
+  it 'exists' do 
+    expect(@cookbook).to be_a(CookBook)
+    expect(@recipe2).to be_a(Recipe)
+  end
+
+end
