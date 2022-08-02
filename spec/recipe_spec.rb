51f6287a834b7b6e23e92fccab79c1f232532d26
Kevin Ta <kvnta@yahoo.com>
2022-08-02 10:22:02 -0600

    Test: ingredients array

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe Recipe do
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
   end
 
   it 'has dem attributes' do 
@@ -21,8 +22,22 @@ RSpec.describe Recipe do
     @recipe1.add_ingredient(@ingredient2, 8)
 
     expect(@recipe1.ingredients_required).to eq({@ingredient1,@ingredient2})
-    
   end
+
+  it 'can list ingredients in a recipe as an array' do 
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    
+    expect(@recipe1.ingredient).to eq([@ingredient1,@ingredient2])
+  end
+
+  
+
+
+
+
+  
 
 
 
