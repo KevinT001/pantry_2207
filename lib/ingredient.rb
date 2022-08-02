92eaeea6b098151748534c5d5e9df49618fb7f08
Kevin Ta <kvnta@yahoo.com>
2022-08-02 09:41:11 -0600

    attr_reader and intialize setup for ingredient

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient
+  attr_reader :name,
+              :unit,
+              :calorie
+  def initialize(info)
+    @name = info[:name] 
+    @unit = info[:unit]
+    @calorie =info[:calorie]
 
-  
+  end
+
 end
